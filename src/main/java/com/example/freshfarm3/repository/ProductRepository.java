9fd0a7bf4402f08b8025c3f13c34dfb66b359e96
Vunisikota Thulasi Shankar Raju Deepa <thulasishankarraju@gmail.com>
2026-07-03 20:59:02 +0530

    Fix ProductRepository updates

--- src/main/java/com/example/freshfarm3/repository/ProductRepository.java
+++ src/main/java/com/example/freshfarm3/repository/ProductRepository.java
@@ -3,7 +3,6 @@ package com.example.freshfarm3.repository;
 import com.example.freshfarm3.entity.Category;
 import com.example.freshfarm3.entity.Farmer;
 import com.example.freshfarm3.entity.Product;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +10,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
@@ -64,4 +64,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     long countByAvailableTrue();
 
     double findAverageProductRating();
+
+    <T> Optional<T> findByCategory_IdAndStatus(Long categoryId, Product.ProductStatus productStatus, Pageable pageable);
 }
